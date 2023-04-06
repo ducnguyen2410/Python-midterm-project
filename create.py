68cb52af78ceddaefc673394c9a1d6dbae4c24e4
nguyen2410 <ducnguyenbernardine@gmail.com>
2023-04-06 02:17:04 -0400

    new

--- create.py
+++ create.py
@@ -1,14 +1,19 @@
 import read
 import os
 import write
+import random
 
 def create_data(new_object):
     if os.path.getsize("db.json") > 0:
         control = read.read_data()
     else:
         control = {}
+    check_id = []
+    check = False
+    for i in control:
+        for j in control[i]:
+            check_id.append(j)
     category_id = new_object["category_id"]
-    id = new_object["id"]
     name = new_object["name"]
     price = new_object["price"]
     stock = new_object["stock"]
@@ -19,13 +24,16 @@ def create_data(new_object):
         print("Product price is not valid. Please try again.")
     elif stock < 0:
         print("Product quantity is not valid. Please try again.")
-    elif int(id) < 0:
-        print("Product ID is invalid. Please try again.")
     elif description == 0:
         print("You must add description for the product.")
     else:
         if str(category_id) not in control:
             control[category_id] = {}
+        while True:
+            id = random.randint(100000, 999999)
+            if str(id) not in check_id:
+                break
+        id = str(id)
         control[category_id][id] = {}
         control[category_id][id]["category_id"] = category_id
         control[category_id][id]["id"] = id
